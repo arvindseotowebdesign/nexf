bfa261a8c99330dd450d1dbe5e0aeeb790555adb
arvindseotowebdesign <arvind.seotowebdesign@gmail.com>
2024-06-11 09:39:26 +0530

    first commit

---
+++ app/blog/page.tsx
@@ -0,0 +1,30 @@
+import { Post } from "../../components/post";
+
+async function getBlogPosts() {
+  const postsResponse = await fetch(
+    "https://brandmenow-yzus.onrender.com/all-blogs",
+    { cache: "no-store" }
+  );
+
+  console.log("Refetching posts");
+
+  const data = await postsResponse.json();
+  return data.blogs;
+}
+
+export default async function Page() {
+  const posts = await getBlogPosts();
+
+  console.log("Rerendering Blog Component");
+
+  return (
+    <div className="flex flex-col items-center pt-10">
+      <h1 className="text-4xl font-bold">Blog</h1>
+      <div className="flex flex-wrap h-full max-w-xl">
+        {posts.map((post: any) => (
+          <Post key={post._id} post={post} />
+        ))}
+      </div>
+    </div>
+  );
+}
